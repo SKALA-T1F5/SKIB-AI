855d01d5c21b645fd4eb9768a4eab5d96afd7eb6
류지혜/Jihye Ryu <domwis.IR@gmail.com>
2025-06-04 15:59:56 +0900

    feat: update main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from api.question.routers.question import router as question_router
+from api.grading.routers.subjective_grading import router as grading_router
 
 app = FastAPI()
 
 app.include_router(question_router)
+app.include_router(grading_router)
