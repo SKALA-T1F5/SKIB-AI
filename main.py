996b218b580adb74dbfbca293033545e97e764c5
류지혜/Jihye Ryu <domwis.IR@gmail.com>
2025-06-09 15:17:10 +0900

    feat: add document api

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from api.question.routers.question import router as question_router
 from api.grading.routers.subjective_grading import router as grading_router
+from api.document.routers.document_upload import router as document_router
 
 app = FastAPI()
 
 app.include_router(question_router)
 app.include_router(grading_router)
+app.include_router(document_router)
